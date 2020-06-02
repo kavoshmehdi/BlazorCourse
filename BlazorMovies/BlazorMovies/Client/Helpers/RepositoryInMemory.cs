58da679ca99efddaf8816e993ed3da1f547623dd
kavoshmehdi <kavoshmehdi@yahoo.com>
2020-06-02 14:02:49 +0430

    init

---
+++ BlazorMovies/BlazorMovies/Client/Helpers/RepositoryInMemory.cs
@@ -0,0 +1,22 @@
+﻿using BlazorMovies.Shared;
+using BlazorMovies.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public class RepositoryInMemory : IRepository
+    {
+        public List<Movie> GetMovies()
+        {
+            return new List<Movie>()
+    {
+                    new Movie(){Title="Spider Man : For Far",ReleaseDate = new DateTime(2015,2,27)},
+                    new Movie(){Title="Inception",ReleaseDate = new DateTime(2011,03,04)},
+                    new Movie(){Title="Star Ware",ReleaseDate=new DateTime(2010,05,04)}
+            };
+        }
+    }
+}
---
+++ BlazorMovies/BlazorMovies/Client/Pages/Counter.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static BlazorMovies.Client.Shared.MainLayout;
+
+namespace BlazorMovies.Client.Pages
+{
+    public partial class Counter
+    {
+        [Inject] SingletonService singleton { get; set; }
+        [Inject] TransientService transient { get; set; }
+        [Inject] IJSRuntime js { get; set; }
+        [CascadingParameter] public AppState AppState { get; set; }
+        private int currentCount = 0;
+        private static int currentCountStatic = 0;
+
+        [JSInvokable]
+        public  async Task IncrementCount()
+        {
+            currentCount++;
+            singleton.Value = currentCount;
+            transient.Value = currentCount;
+            currentCountStatic++;
+            await js.InvokeVoidAsync("dotnetStaticInvokation");
+        }
+        public async Task IncrementCountJavascript()
+        {
+            await js.InvokeVoidAsync("dotnetInstanceInvokation",
+                DotNetObjectReference.Create(this));
+        }
+        [JSInvokable]
+        public static Task<int> GetCounter()
+        {
+            return Task.FromResult(currentCountStatic);
+        }
+
+    }
+}
