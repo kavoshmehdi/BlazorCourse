fafdf771f7bb874e7675559ba06e7e1fdeac133a
kavoshmehdi <kavoshmehdi@yahoo.com>
2020-06-06 07:46:18 +0430

    init

---
+++ BlazorMovies/Module 5 - Forms/BlazorMovies/Client/wwwroot/js/Utilities.js
@@ -0,0 +1,12 @@
+﻿function my_function(message) {
+    console.log("from utilities " + message);
+}
+function dotnetStaticInvokation() {
+    DotNet.invokeMethodAsync("BlazorMovies.Client", "GetCounter")
+        .then(result => {
+            console.log("counter from javascript : " + result);
+        });
+}
+function dotnetInstanceInvokation(dotnetHelper) {
+    dotnetHelper.invokeMethodAsync("IncrementCount");
+}
